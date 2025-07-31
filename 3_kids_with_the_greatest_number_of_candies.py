4de1d284050bea2b65be0c1a14002e965c908bcc
Shreya-Kumbar <shreyarkumbar@gmail.com>
2025-07-31 10:02:49 +0530

    Added Day02

--- 3_kids_with_the_greatest_number_of_candies.py
+++ 3_kids_with_the_greatest_number_of_candies.py
@@ -1,5 +1,5 @@
-#LeetCode 1431: Kids With the Greatest Number of Candies
-#https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/
+# LeetCode 1431: Kids With the Greatest Number of Candies
+# https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/
 
 class Solution:
     def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
