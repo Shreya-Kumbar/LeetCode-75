40d9247199a83ab1e8c4c45dcdac2368f11ed8e3
Shreya-Kumbar <shreyarkumbar@gmail.com>
2025-07-31 10:06:59 +0530

    Added Day03

--- 3_kids_with_the_greatest_number_of_candies.py
+++ 3_kids_with_the_greatest_number_of_candies.py
@@ -1,4 +1,4 @@
-# LeetCode 1431: Kids With the Greatest Number of Candies
+# LeetCode 1431: Kids with the Greatest Number of Candies
 # https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/
 
 class Solution:
