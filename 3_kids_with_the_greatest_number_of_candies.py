61636ca1634335e1e27377c3d55037b0dd06d584
Shreya-Kumbar <shreyarkumbar@gmail.com>
2025-07-31 09:59:15 +0530

    Added Day03

---
+++ 3_kids_with_the_greatest_number_of_candies.py
@@ -0,0 +1,20 @@
+#LeetCode 1431: Kids With the Greatest Number of Candies
+#https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/
+
+class Solution:
+    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
+        result = []
+        MaxSoFar = 0
+
+        for item in candies:
+            if item > MaxSoFar:
+                MaxSoFar = item
+
+        for numberOfCandies in candies:
+            if numberOfCandies + extraCandies >= MaxSoFar:
+                result.append(True)
+
+            else:
+                result.append(False)
+
+        return result
