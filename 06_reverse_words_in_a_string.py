ce557df1d9d6f687e79e4830ef5029d798e28520
Shreya-Kumbar <shreyarkumbar@gmail.com>
2025-09-07 10:35:06 +0530

    Modified file

--- 06_reverse_words_in_a_string.py
+++ 06_reverse_words_in_a_string.py
@@ -4,4 +4,25 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
         
-        return ' '.join(s.split()[::-1])
+        s = s.strip()    # remove extra spaces
+
+        i = j = len(s) - 1
+        # start from the end
+
+        result = []
+
+        while i >= 0:
+
+            # we take the whole word starting from end
+            while i >= 0 and s[i] != " ":
+                i -= 1
+
+            result.append(s[ i+1 : j+1 ])
+
+            # now skip the space and do same for next word.
+            while i >= 0 and s[i] == " ":
+                i -= 1
+            
+            j = i
+
+        return " ".join(result)
